3a7e6ae8f89ae96bf4cf58eeae9c12d3d94087b9
super-archive-1419 <sophzsul@gmail.com>
2020-01-16 08:15:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(){
   kittens.push('Ralph');
   return kittens}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob');
+  return kittens
+}
